7e24856c6e098109931b143fe9a2b8535c3e0f30
jaime <jaime.boukhrij@smartick.com>
2024-05-20 12:38:11 +0200

    img

--- src/components/Projects/Card.jsx
+++ src/components/Projects/Card.jsx
@@ -11,7 +11,7 @@ export default function Card () {
           target='_blank'
           key={name} className='p-5 border border-primary flex flex-col gap-5 max-w-[500px] hover:bg-primary/5 hover:scale-105 transition-all'
         >
-          <img className='w-full h-auto max-w-[230px] m-auto' src={`http://localhost:5173/src/assets/${img}.jpg`} alt='' />
+          <img className='w-full h-auto max-w-[230px] m-auto' src={`../../src/assets/${img}.jpg`} alt='' />
           <h5 className='text-2xl font-semibold text-center my-5'>{name}</h5>
           <p className='dark:text-gray-400  text-lg  mb-5'>{translated ? descriptionTrans : descriptionOrg}</p>
           <div className='flex items-center justify-center flex-wrap gap-3 mt-auto w-full'>
